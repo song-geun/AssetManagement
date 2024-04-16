12c00067403e796532764392e8171809a640861a
song-geun <brownmole01@naver.com>
2024-04-16 15:15:07 +0900

    init

---
+++ src/test/java/MSHC/com/AssetManagement/AssetManagementApplicationTests.java
@@ -0,0 +1,11 @@
+package MSHC.com.AssetManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagementApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
